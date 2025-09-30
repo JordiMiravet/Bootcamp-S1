f123e3d5b3198196ca8843a24fcc0285f9341aa8
Jordi Miravet <jordimiravet.dev@gmail.com>
2025-09-30 16:39:01 +0200

    Añadida estructura inicial de navbar con toggle menu

--- js/main.js
+++ js/main.js
@@ -4,6 +4,5 @@ const toggleBtn = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 toggleBtn.addEventListener("click", () => {
-    toggleBtn.classList.toggle("open");
-    navLinks.classList.toggle("show");
+  navLinks.classList.toggle("show");
 });
